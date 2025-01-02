ddc451029c93db45078271a628ea33eb36f3bbd4
Ruletk <damir.timergazin@gmail.com>
2025-01-02 07:00:25 +0500

    feat: Added all routes. feat: Added dockerfile. feat: Updated docker-compose file.

--- apps/warehouse/src/main.ts
+++ apps/warehouse/src/main.ts
@@ -1,21 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
+import { healthRouter } from './routes/healthRouter';
+import { warehouseRouter } from './routes/warehouseRouter';
 
 const app = express();
 
+app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to warehouse!' });
-});
+app.use('/', warehouseRouter);
+app.use('/health', healthRouter);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
