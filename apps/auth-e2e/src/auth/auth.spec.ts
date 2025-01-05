0c19a3ace5569ae33193a570fd8882edba385c67
Ruletk <damir.timergazin@gmail.com>
2025-01-05 08:15:42 +0500

    test: Fixed e2e test configuration.

--- apps/auth-e2e/src/auth/auth.spec.ts
+++ apps/auth-e2e/src/auth/auth.spec.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
+describe('GET /health', () => {
+  it('should return a health message', async () => {
+    const res = await axios.get(`/health`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toHaveProperty('status');
+    expect(res.data.status).toBe('OK');
+
+    expect(res.data).toHaveProperty('timestamp');
+    expect(typeof res.data.timestamp).toBe('string');
   });
 });
