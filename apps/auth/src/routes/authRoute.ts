450942b09737e32134ef176de568f396d3d62351
Ruletk <damir.timergazin@gmail.com>
2025-01-18 17:14:09 +0500

    feat(auth): WAR-25 WAR-26 Added validation middleware for request DTOs in authentication routes.

--- apps/auth/src/routes/authRoute.ts
+++ apps/auth/src/routes/authRoute.ts
@@ -1,14 +1,36 @@
 import { Router } from 'express';
 import { AuthHandlers } from './authHandlers';
+import {
+  AuthRequest,
+  PasswordChange,
+  RequestPasswordChange,
+} from '../dto/request';
+import { validateRequest } from '../middleware/validationMiddleware';
 
-const authHandlers = new AuthHandlers("test dependency");
+const authHandlers = new AuthHandlers('test dependency');
 
 const authRouter = Router();
 
-authRouter.post('/login', authHandlers.loginHandler);
-authRouter.post('/register', authHandlers.registerHandler);
+authRouter.post(
+  '/login',
+  validateRequest(AuthRequest),
+  authHandlers.loginHandler
+);
+authRouter.post(
+  '/register',
+  validateRequest(AuthRequest),
+  authHandlers.registerHandler
+);
 authRouter.get('/logout', authHandlers.logoutHandler);
-authRouter.post('/reset-password', authHandlers.resetPasswordHandler);
-authRouter.post('/change-password/:token', authHandlers.changePasswordHandler);
+authRouter.post(
+  '/reset-password',
+  validateRequest(RequestPasswordChange),
+  authHandlers.resetPasswordHandler
+);
+authRouter.post(
+  '/change-password/:token',
+  validateRequest(PasswordChange),
+  authHandlers.changePasswordHandler
+);
 
 export { authRouter };
