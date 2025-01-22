3fd70f8b9d937a40de85c39d9d350d0936fb5426
AkBe <154293625+AkBePEEK@users.noreply.github.com>
2025-01-23 02:44:39 +0500

    feat(warehouse): Added validation for endpoints.

--- apps/warehouse/src/routes/warehouseRouter.ts
+++ apps/warehouse/src/routes/warehouseRouter.ts
@@ -1,40 +1,105 @@
 import { Router } from "express";
+import {validateRequest} from "@warehouse/validation";
+import {
+  AssignUserRoleRequest,
+  CreateRoleRequest,
+  CreateTagRequest,
+  CreateWarehouseRequest, RemoveUserRoleRequest, UpdateRoleRequest,
+  UpdateTagRequest,
+  UpdateWarehouseRequest
+} from "../dto/request";
 
 
 const router = Router();
 
-router.get('/list', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).send({
-    message: 'List of warehouses',
+    message: 'Hello World',
   });
-});
+})
 
-router.post('/list', (req, res) => {
+router.post('/', validateRequest(CreateWarehouseRequest), (req, res) => {
   res.status(201).send({
-    message: 'Warehouse added',
+    message: 'Hello World',
   });
+})
 
-});
-
-router.get('/:token', (req, res) => {
-  const token = req.params.token;
+router.put('/:id', validateRequest(UpdateWarehouseRequest), (req, res) => {
   res.status(200).send({
-    message: `Warehouse details with token ${token}`,
+    message: 'Hello World',
   });
-});
+})
 
-router.put('/:token', (req, res) => {
-  const token = req.params.token;
+router.delete('/:id', (req, res) => {
   res.status(200).send({
-    message: `Warehouse updated with token ${token}`,
+    message: 'Hello World',
   });
-});
+})
 
-router.delete('/:token', (req, res) => {
-  const token = req.params.token;
+router.get('/:id', (req, res) => {
   res.status(200).send({
-    message: `Warehouse deleted with token ${token}`,
+    message: 'Hello World',
   });
-});
+})
+
+router.get('/tags', (req, res) => {
+  res.status(200).send({
+    message: 'Hello World',
+  });
+})
+
+router.post('/tags', validateRequest(CreateTagRequest), (req, res) => {
+  res.status(201).send({
+    message: 'Hello World',
+  });
+})
+
+router.put('/tags/:id', validateRequest(UpdateTagRequest), (req, res) => {
+  res.status(200).send({
+    message: 'Hello World',
+  });
+})
+
+router.delete('/tags/:id', (req, res) => {
+  res.status(200).send({
+    message: 'Hello World',
+  });
+})
+
+router.get('/roles', (req, res) => {
+  res.status(200).send({
+    message: 'Hello World',
+  });
+})
+
+router.post('/roles', validateRequest(CreateRoleRequest), (req, res) => {
+  res.status(201).send({
+    message: 'Hello World',
+  })
+})
+
+router.put('/roles/:id', validateRequest(UpdateRoleRequest), (req, res) => {
+  res.status(200).send({
+    message: 'Hello World',
+  });
+})
+
+router.delete('/roles/:id', (req, res) => {
+  res.status(200).send({
+    message: 'Hello World',
+  });
+})
+
+router.post('/roles/:id/user', validateRequest(AssignUserRoleRequest), (req, res) => {
+  res.status(201).send({
+    message: 'Hello World',
+  });
+})
+
+router.delete('/roles/:id/user', validateRequest(RemoveUserRoleRequest), (req, res) => {
+  res.status(200).send({
+    message: 'Hello World',
+  });
+})
 
 export { router as warehouseRouter };
