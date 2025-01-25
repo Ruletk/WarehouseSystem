43c6e1e9334e37bbc8566dcaf1da011356af8658
AkBe <154293625+AkBePEEK@users.noreply.github.com>
2025-01-25 20:17:33 +0500

    feat(warehouse): WAR-60 Added get / endpoint
    # Conflicts:
    #	apps/warehouse/src/routes/warehouseRouter.ts

---
+++ apps/warehouse/src/services/warehouseService.ts
@@ -0,0 +1,85 @@
+import { WarehouseRepository } from '../repositories/warehouseRepository';
+import {
+  AssignUserRoleRequest,
+  CreateRoleRequest,
+  CreateTagRequest,
+  CreateWarehouseRequest,
+  UpdateRoleRequest,
+  UpdateTagRequest,
+  UpdateWarehouseRequest,
+} from '../dto/request';
+import {
+  WarehouseListResponse,
+  WarehouseResponse,
+  WarehouseRoleListResponse,
+  WarehouseRoleResponse,
+  WarehouseTagListResponse,
+  WarehouseTagResponse,
+  WarehouseUserRoleResponse,
+} from '../dto/response';
+import { ApiResponse } from '@warehouse/validation';
+
+export class WarehouseService {
+  private warehouseRepository: WarehouseRepository;
+
+  constructor(warehouseRepository: WarehouseRepository) {
+    this.warehouseRepository = warehouseRepository;
+  }
+
+  public async createWarehouse(
+    req: CreateWarehouseRequest
+  ): Promise<WarehouseResponse | ApiResponse> {
+    try {
+      // Create a new warehouse instance
+      const warehouse = await this.warehouseRepository.create(
+        req.name,
+        req.latitude,
+        req.longitude,
+        req.address
+      );
+
+      return WarehouseResponse.from({
+        id: warehouse.id,
+        name: warehouse.name,
+        latitude: warehouse.latitude,
+        longitude: warehouse.longitude,
+        address: warehouse.address,
+        createdAt: warehouse.createdAt,
+        updatedAt: warehouse.updatedAt,
+      });
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
+  public async getAllWarehouses(): Promise<
+    WarehouseListResponse | ApiResponse
+  > {
+    try {
+      // Получаем все склады из репозитория
+      const warehouses = await this.warehouseRepository.findAll();
+
+      // Преобразуем данные в формат WarehouseResponse
+      const warehouseResponses = warehouses.map((warehouse) =>
+        WarehouseResponse.from({
+          id: warehouse.id,
+          name: warehouse.name,
+          latitude: warehouse.latitude,
+          longitude: warehouse.longitude,
+          address: warehouse.address,
+          createdAt: warehouse.createdAt,
+          updatedAt: warehouse.updatedAt,
+        })
+      );
+
+      // Возвращаем результат
+      return WarehouseListResponse.from({ warehouses: warehouseResponses });
+    } catch (error) {
+      console.error('Ошибка при получении складов:', error);
+      return ApiResponse.from({
+        message: 'Не удалось получить список складов.',
+      });
+    }
+  }
+}
