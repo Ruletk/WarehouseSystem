4e5883afefb0c496ef874be582c79566fd8eea7f
Ruletk <damir.timergazin@gmail.com>
2025-01-29 21:02:20 +0500

    feat(warehouse): Refactor warehouse routes into WarehouseAPI class and initialize function in main.

--- apps/warehouse/src/routes/warehouseRouter.ts
+++ apps/warehouse/src/routes/warehouseRouter.ts
@@ -10,96 +10,153 @@ import {
 } from "../dto/request";
 
 
-const router = Router();
+export class WarehouseAPI {
+  // # Uncomment the following lines to enable dependency injection
 
-router.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  // private warehouseService: WarehouseService;
+  // private warehouseTagService: WarehouseTagService;
+  // private warehouseUserService: WarehouseUserService;
+  constructor(
+    // warehouseService: WarehouseService,
+    // warehouseTagService: WarehouseTagService,
+    // warehouseUserService: WarehouseUserService
+  ) {
+    console.log('INFO: Creating WarehouseAPI instance');
+    // this.warehouseService = warehouseService;
+    // this.warehouseTagService = warehouseTagService;
+    // this.warehouseUserService = warehouseUserService;
+  }
 
-router.post('/', validateRequest(CreateWarehouseRequest), (req, res) => {
-  res.status(201).send({
-    message: 'Hello World',
-  });
-})
+  registerRoutes(router: Router): void {
+    console.log('INFO: Registering warehouse routes');
+    this.registerWarehouseRoutes(router);
+    this.registerTagRoutes(router);
+    this.registerRoleRoutes(router);
+  }
 
-router.put('/:id', validateRequest(UpdateWarehouseRequest), (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  private registerWarehouseRoutes(router: Router): void {
+    console.log(
+      `INFO: Registering warehouse routes with router: ${router.name}.`
+    );
 
-router.delete('/:id', (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+    router.get('/', this.getWarehousesHandler.bind(this));
+    router.post('/', validateRequest(CreateWarehouseRequest), this.createWarehouseHandler.bind(this));
+    router.put('/:id', validateRequest(UpdateWarehouseRequest), this.updateWarehouseHandler.bind(this));
+    router.delete('/:id', this.deleteWarehouseHandler.bind(this));
+    router.get('/:id', this.getWarehouseHandler.bind(this));
+  }
 
-router.get('/:id', (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  private registerTagRoutes(router: Router): void {
+    console.log(
+      `INFO: Registering tag routes with router: ${router.name}.`
+    );
 
-router.get('/tags', (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+    router.get('/tags', this.getTagsHandler.bind(this));
+    router.post('/tags', validateRequest(CreateTagRequest), this.createTagHandler.bind(this));
+    router.put('/tags/:id', validateRequest(UpdateTagRequest), this.updateTagHandler.bind(this));
+    router.delete('/tags/:id', this.deleteTagHandler.bind(this));
+  }
 
-router.post('/tags', validateRequest(CreateTagRequest), (req, res) => {
-  res.status(201).send({
-    message: 'Hello World',
-  });
-})
+  private registerRoleRoutes(router: Router): void {
+    console.log(
+      `INFO: Registering role routes with router: ${router.name}.`
+    );
 
-router.put('/tags/:id', validateRequest(UpdateTagRequest), (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+    router.get('/roles', this.getRolesHandler.bind(this));
+    router.post('/roles', validateRequest(CreateRoleRequest), this.createRoleHandler.bind(this));
+    router.put('/roles/:id', validateRequest(UpdateRoleRequest), this.updateRoleHandler.bind(this));
+    router.delete('/roles/:id', this.deleteRoleHandler.bind(this));
+    router.post('/roles/:id/user', validateRequest(AssignUserRoleRequest), this.assignUserRoleHandler.bind(this));
+    router.delete('/roles/:id/user', validateRequest(RemoveUserRoleRequest), this.removeUserRoleHandler.bind(this));
+  }
 
-router.delete('/tags/:id', (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  private getWarehousesHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
 
-router.get('/roles', (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  private createWarehouseHandler(req, res) {
+    res.status(201).send({
+      message: 'Hello World',
+    });
+  }
 
-router.post('/roles', validateRequest(CreateRoleRequest), (req, res) => {
-  res.status(201).send({
-    message: 'Hello World',
-  })
-})
+  private updateWarehouseHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
 
-router.put('/roles/:id', validateRequest(UpdateRoleRequest), (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  private deleteWarehouseHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
 
-router.delete('/roles/:id', (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  private getWarehouseHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
 
-router.post('/roles/:id/user', validateRequest(AssignUserRoleRequest), (req, res) => {
-  res.status(201).send({
-    message: 'Hello World',
-  });
-})
+  private getTagsHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
 
-router.delete('/roles/:id/user', validateRequest(RemoveUserRoleRequest), (req, res) => {
-  res.status(200).send({
-    message: 'Hello World',
-  });
-})
+  private createTagHandler(req, res) {
+    res.status(201).send({
+      message: 'Hello World',
+    });
+  }
 
-export { router as warehouseRouter };
+  private updateTagHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
+
+  private deleteTagHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
+
+  private getRolesHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
+
+  private createRoleHandler(req, res) {
+    res.status(201).send({
+      message: 'Hello World',
+    });
+  }
+
+  private updateRoleHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
+
+  private deleteRoleHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
+
+  private assignUserRoleHandler(req, res) {
+    res.status(201).send({
+      message: 'Hello World',
+    });
+  }
+
+  private removeUserRoleHandler(req, res) {
+    res.status(200).send({
+      message: 'Hello World',
+    });
+  }
+}
