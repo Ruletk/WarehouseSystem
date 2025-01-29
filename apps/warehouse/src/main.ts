129839672f5bbce8b8c045d1973b85c0552c732a
Ruletk <damir.timergazin@gmail.com>
2025-01-29 21:02:20 +0500

    feat(warehouse): Refactor warehouse routes into WarehouseAPI class and initialize function in main.

--- apps/warehouse/src/main.ts
+++ apps/warehouse/src/main.ts
@@ -1,14 +1,50 @@
 import express from 'express';
 import * as path from 'path';
 import { healthRouter } from './routes/healthRouter';
-import { warehouseRouter } from './routes/warehouseRouter';
+import { WarehouseAPI } from './routes/warehouseRouter';
+import { connectDB } from './config/ds';
+import { WarehouseRepository } from './repositories/warehouseRepository';
+import { WarehouseTagRepository } from './repositories/warehouseTagRepository';
+import { WarehouseUserRepository } from './repositories/warehouseUserRepository';
 
 const app = express();
 
 app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use('/', warehouseRouter);
-app.use('/health', healthRouter);
+
+
+
+async function main() {
+  // Initialize Dependencies
+  const DB = await connectDB();
+
+  // Initialize Repositories
+  const warehouseRepository = new WarehouseRepository(DB);
+  const warehouseTagRepository = new WarehouseTagRepository(DB);
+  const warehouseUserRepository = new WarehouseUserRepository(DB);
+  // To prevent unused variable error
+  console.log(!!warehouseRepository, !!warehouseTagRepository, !!warehouseUserRepository);
+  // True if all are initialized
+
+  // Initialize Services
+
+  // Initialize APIs
+  const warehouseAPI = new WarehouseAPI();
+
+  // Initialize Routers
+  const warehouseRouter = express.Router();
+  warehouseAPI.registerRoutes(warehouseRouter);
+
+
+  // Register Routers
+  app.use('/', warehouseRouter);
+  app.use('/health', healthRouter);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
