ecf02b5a3eb8dba77c32a118eda19a66de645fc9
Adil_Forest <148645840+Adilforest@users.noreply.github.com>
2025-02-03 22:19:05 +0500

    feat(frontend): WAR-84 Creation of basic pages for Auth service.
    Creation of styles for pages for Auth service.

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -22,6 +22,7 @@ export function App() {
         </ul>
       </div>
       <Routes>
+        <Route path="/" element={<div>Welcome frontend</div>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
