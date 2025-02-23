9c620d897b6b876d990d082872a924c51b1fe237
Ruletk <damir.timergazin@gmail.com>
2025-02-23 22:19:55 +0500

    docs(rpc): Added documentation to `RabbitMQClient` class. test(rpc): Added tests for `RabbitMQClient` class.

--- shared/rpc/src/lib/rpc.ts
+++ shared/rpc/src/lib/rpc.ts
@@ -33,6 +33,14 @@ interface RpcOptions {
   timeout?: number;
 }
 
+/**
+ * RabbitMQClient is a class that provides an interface for connecting to a RabbitMQ server,
+ * publishing messages, subscribing to queues, and handling RPC requests and responses.
+ * It extends EventEmitter to allow for event-driven programming.
+ *
+ * @class RabbitMQClient
+ * @extends {EventEmitter}
+ */
 export class RabbitMQClient extends EventEmitter {
   private connection: amqp.Connection | null = null;
   private channel: amqp.Channel | null = null;
@@ -54,6 +62,12 @@ export class RabbitMQClient extends EventEmitter {
     };
   }
 
+  /**
+   * Initializes the RabbitMQ client by establishing a connection and setting up the RPC response queue.
+   * Logs the initialization process.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the initialization is complete.
+   */
   public async init(): Promise<void> {
     logger.info('Initializing RabbitMQ client');
     await this.connect();
@@ -61,6 +75,17 @@ export class RabbitMQClient extends EventEmitter {
     logger.info('RabbitMQ client initialized successfully');
   }
 
+  /**
+   * Establishes a connection to RabbitMQ if not already connected.
+   * 
+   * This method attempts to connect to RabbitMQ using the provided configuration URL.
+   * It sets up event listeners for connection close and error events, and creates a channel
+   * for communication. If the connection is successful, it emits a 'connected' event and logs
+   * the success. In case of an error, it logs the failure and handles the connection error.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the connection is successfully established.
+   * @throws {Error} If there is an error during the connection process.
+   */
   private async connect(): Promise<void> {
     if (this.connection || this.isConnecting) return;
     this.isConnecting = true;
@@ -86,7 +111,23 @@ export class RabbitMQClient extends EventEmitter {
     }
   }
 
-  // Создаём временную очередь для RPC-ответов и сохраняем её имя
+  
+  /**
+   * Sets up the RPC response queue for handling responses from RPC calls.
+   * 
+   * This method asserts a new exclusive queue for receiving RPC responses and
+   * sets up a consumer to handle incoming messages. Each message is expected to
+   * have a correlation ID that matches a callback stored in `rpcCallbacks`.
+   * 
+   * If the message content is valid JSON, the corresponding callback is invoked
+   * with the parsed data. If the JSON parsing fails, the callback is invoked with
+   * an error object. After handling the message, it acknowledges the message to
+   * the channel.
+   * 
+   * @throws {Error} If the channel is not available.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the setup is complete.
+   */
   private async setupRpcResponseQueue(): Promise<void> {
     if (!this.channel) throw new Error('Channel not available');
 
@@ -116,6 +157,13 @@ export class RabbitMQClient extends EventEmitter {
     });
   }
 
+  /**
+   * Attempts to reconnect to RabbitMQ with a delay specified in the configuration.
+   * If the maximum number of retries is reached, an error is logged and emitted.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the reconnection attempt is complete.
+   * @throws {Error} If the maximum number of reconnection attempts is reached.
+   */
   private async reconnect(): Promise<void> {
     if (this.retryCount >= (this.config.maxRetries || 10)) {
       logger.error('Max reconnection attempts reached');
@@ -140,6 +188,17 @@ export class RabbitMQClient extends EventEmitter {
     }
   }
 
+  /**
+   * Handles the closure of the RabbitMQ connection.
+   * 
+   * This method performs the following actions:
+   * - Logs a warning message indicating that the RabbitMQ connection has closed.
+   * - Emits a 'disconnected' event.
+   * - Sets the connection, channel, and rpcReplyQueue properties to null.
+   * - Attempts to reconnect by calling the `reconnect` method.
+   * 
+   * @private
+   */
   private handleConnectionClose(): void {
     logger.warn('RabbitMQ connection closed');
     this.emit('disconnected');
@@ -149,6 +208,12 @@ export class RabbitMQClient extends EventEmitter {
     this.reconnect();
   }
 
+  /**
+   * Handles connection errors by logging the error, emitting an 'error' event,
+   * and attempting to reconnect to RabbitMQ.
+   *
+   * @param error - The error object representing the connection error.
+   */
   private handleConnectionError(error: Error): void {
     logger.error('RabbitMQ connection error:', error);
     this.emit('error', error);
@@ -187,6 +252,28 @@ export class RabbitMQClient extends EventEmitter {
     return result;
   }
 
+  /**
+   * Subscribes to a message queue with the given options and handler.
+   * 
+   * @template T - The type of the message content.
+   * @param {SubscribeOptions} options - The subscription options.
+   * @param {(msg: T) => Promise<void>} handler - The handler function to process received messages.
+   * @returns {Promise<void>} A promise that resolves when the subscription is set up.
+   * @throws {Error} If the channel is not available.
+   * 
+   * @example
+   * ```typescript
+   * const options: SubscribeOptions = {
+   *   exchange: 'my_exchange',
+   *   queue: 'my_queue',
+   *   routingKey: 'my_routing_key',
+   * };
+   * 
+   * await rpc.subscribe(options, async (msg) => {
+   *   console.log('Received message:', msg);
+   * });
+   * ```
+   */
   public async subscribe<T>(
     options: SubscribeOptions,
     handler: (msg: T) => Promise<void>
@@ -246,7 +333,18 @@ export class RabbitMQClient extends EventEmitter {
     });
   }
 
-  // RPC-запрос: отправляем сообщение на указанную очередь и ждем ответа в rpcReplyQueue
+  
+  /**
+   * Sends an RPC request to the specified queue with the given message and options.
+   * 
+   * @template T - The expected response type.
+   * @param {string} queue - The name of the queue to send the request to.
+   * @param {object} message - The message to send in the request.
+   * @param {RpcOptions} [options={}] - Optional settings for the RPC request.
+   * @param {number} [options.timeout=5000] - The timeout duration in milliseconds for the RPC request.
+   * @returns {Promise<T>} - A promise that resolves with the response of type T.
+   * @throws {Error} - Throws an error if the channel or RPC reply queue is not available, or if the request times out.
+   */
   public async rpcRequest<T>(
     queue: string,
     message: object,
@@ -292,7 +390,23 @@ export class RabbitMQClient extends EventEmitter {
     });
   }
 
-  // RPC-воркер: обрабатываем запросы из очереди и отправляем ответ, если указаны replyTo и correlationId
+  
+  /**
+   * Creates an RPC worker that listens to a specified queue and processes messages using the provided handler.
+   *
+   * @param queueName - The name of the queue to listen to.
+   * @param handler - A function that processes incoming messages and returns a promise with the result.
+   * @returns A promise that resolves when the RPC worker is successfully created.
+   * @throws Will throw an error if the channel is not available.
+   *
+   * @example
+   * ```typescript
+   * await createRpcWorker('myQueue', async (msg) => {
+   *   // Process the message
+   *   return { success: true };
+   * });
+   * ```
+   */
   public async createRpcWorker(
     queueName: string,
     handler: (msg: Record<string, unknown>) => Promise<unknown>
@@ -341,6 +455,14 @@ export class RabbitMQClient extends EventEmitter {
     });
   }
 
+  /**
+   * Closes the RabbitMQ client connection.
+   * 
+   * This method will close the channel and connection if they are open,
+   * emit a 'closed' event, and log the closure process.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the connection is closed.
+   */
   public async close(): Promise<void> {
     logger.info('Closing RabbitMQ client connection');
     if (this.channel) await this.channel.close();
